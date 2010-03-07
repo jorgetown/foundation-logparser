97c7d4e055183937e26618ddfecd81b031dcf76d
jdecastro <jorgetown@gmail.com>
2010-03-07 19:22:58 +0000

    missing unit-tests

---
+++ src/test/java/org/logparser/SamplingByTimeTest.java
@@ -0,0 +1,15 @@
+package org.logparser;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link SamplingByTime}.
+ * 
+ * @author jorge.decastro
+ * 
+ */
+public class SamplingByTimeTest {
+	@Test
+	public void testNullMessageFilter() {
+	}
+}
