ddbddc60e7f642d6e3530e716dda497ffd54d7c6
jdecastro <jorgetown@gmail.com>
2010-03-07 19:03:40 +0000

    missing ignore annotation for junit

--- src/test/java/org/logparser/TestMessage.java
+++ src/test/java/org/logparser/TestMessage.java
@@ -1,11 +1,14 @@
 package org.logparser;
 
+import org.junit.Ignore;
+
 /**
  * Represents a simple log message used for testing.
  * 
  * @author jorge.decastro
  * 
  */
+@Ignore
 public class TestMessage implements IStatsCapable {
 	private final long milliseconds;
 
