32af8177f2e181fc1462d8979af46ea69fa2c1db
jdecastro <jorgetown@gmail.com>
2010-07-22 17:30:13 +0100

    Added original log entry text signature; updated json output

--- src/main/java/org/logparser/ITimestampedEntry.java
+++ src/main/java/org/logparser/ITimestampedEntry.java
@@ -8,8 +8,15 @@ import org.codehaus.jackson.annotate.JsonPropertyOrder;
  * @author jorge.decastro
  * 
  */
-@JsonPropertyOrder( { "timestamp", "action", "duration" })
-public interface ITimestampedEntry {
+@JsonPropertyOrder( { "timestamp", "action", "duration", "text" })
+public interface ITimestampedEntry extends IJsonSerializable, ICsvSerializable {
+	/**
+	 * The original log entry text.
+	 * 
+	 * @return a {@link String} containing the original log entry text.
+	 */
+	public String getText();
+	
 	/**
 	 * The timestamp of the log entry.
 	 * 
@@ -27,7 +34,7 @@ public interface ITimestampedEntry {
 	/**
 	 * The time taken by the log action being measured.
 	 * 
-	 * @return a {@code double} representing the duration of the action.
+	 * @return a {@code Number} representing the duration of the action.
 	 */
 	public double getDuration();
 }
