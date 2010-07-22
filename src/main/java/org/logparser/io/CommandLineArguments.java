4d59e46da699665a9d8f9923174bdb23bf40c4b3
jdecastro <jorgetown@gmail.com>
2010-07-22 14:30:52 +0100

    Introduced jcommander to parse command line arguments

---
+++ src/main/java/org/logparser/io/CommandLineArguments.java
@@ -0,0 +1,18 @@
+package org.logparser.io;
+
+import com.beust.jcommander.Parameter;
+
+/**
+ * Represents available command line arguments.
+ * 
+ * @see <a href="http://beust.com/jcommander/">JCommander</a>
+ * @author jorge.decastro
+ * 
+ */
+public class CommandLineArguments {
+	@Parameter(names = { "-file", "-f" }, description = "Optional path to JSON configuration file; defaults to 'config.json' in the current directory", required = false)
+	public String configFile = "config.json";
+
+	@Parameter(names = { "-log", "-l" }, description = "Name of desired log group to analyze", required = true)
+	public String logName;
+}
