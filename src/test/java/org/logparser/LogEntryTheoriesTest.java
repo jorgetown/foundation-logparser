00cde55914c0a57a77e56e5bce5a71e4c4ce3ccf
jdecastro <jorgetown@gmail.com>
2010-08-23 23:09:47 +0100

    Renamed so that theory is captured by surefire's default naming pattern for tests

---
+++ src/test/java/org/logparser/LogEntryTheoriesTest.java
@@ -0,0 +1,37 @@
+package org.logparser;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.experimental.theories.DataPoint;
+
+/**
+ * Using {@link LogEntry} data points to test adherence to Object's equals and hashcode contracts.
+ * 
+ * @author jorge.decastro
+ * 
+ */
+public class LogEntryTheoriesTest extends ObjectTheories {
+	private static final String ENTRY = "10.118.101.132 - - [15/Dec/2008:17:15:00 +0000] \"POST /statusCheck.do HTTP/1.1\" 200 1779 2073";
+	private static final String ACTION = "statusCheck.do";
+	private static final String DURATION = "2073.0";
+	private static Calendar calendar = Calendar.getInstance();
+	private static Date date;
+	
+	static {
+		calendar.set(2008, 11, 15, 17, 15, 00);
+		date = calendar.getTime();
+	}
+
+	@DataPoint
+	public static LogEntry nullEntry = null;
+	@DataPoint
+	public static LogEntry x = new LogEntry(ENTRY, date, ACTION, DURATION);
+	@DataPoint
+	public static LogEntry y = new LogEntry(ENTRY, date, ACTION, DURATION);
+	@DataPoint
+	public static LogEntry z = x;
+	@DataPoint
+	public static LogEntry w = new LogEntry(
+			"10.118.101.132 - - [15/Dec/2008:17:15:00 +0000] \"POST /edit.do HTTP/1.1\" 200 1779 1100", date, "edit.do", "1100.0");
+}
