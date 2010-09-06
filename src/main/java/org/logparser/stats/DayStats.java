b46c664683129229b85166055c7dc2db4a9c7c11
jdecastro <jorgetown@gmail.com>
2010-09-06 23:12:30 +0100

    Added api to filter daily stats entries via predicates

--- src/main/java/org/logparser/stats/DayStats.java
+++ src/main/java/org/logparser/stats/DayStats.java
@@ -13,6 +13,7 @@ import net.jcip.annotations.Immutable;
 import org.apache.commons.lang.NotImplementedException;
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.math.stat.descriptive.StatisticalSummary;
+import org.apache.commons.math.stat.descriptive.SummaryStatistics;
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.annotate.JsonIgnore;
 import org.codehaus.jackson.annotate.JsonPropertyOrder;
@@ -23,6 +24,7 @@ import org.logparser.IJsonSerializable;
 import org.logparser.ITimestampedEntry;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
 
 /**
  * Provides a statistical summary of a collection of log
@@ -62,6 +64,27 @@ public class DayStats<E extends ITimestampedEntry> extends AbstractStats<E> impl
 			timeStats = new TimeStats<E>();
 		}
 		return timeStats;
+	}
+
+	public Map<String, TimeStats<E>> filter(final Predicate<PredicateArguments> predicate) {
+		Map<String, TimeStats<E>> filtered = new TreeMap<String, TimeStats<E>>();
+		SummaryStatistics movingStats;
+		StatisticalSummary currentStats;
+		PredicateArguments arguments;
+		for (Entry<String, TimeStats<E>> entries : dayStats.entrySet()) {
+			movingStats = new SummaryStatistics();
+			for (Entry<Integer, StatisticalSummary> times : entries.getValue().getTimeStats().entrySet()) {
+				currentStats = times.getValue();
+				movingStats.addValue(currentStats.getMean()); // calculate moving average
+				arguments = new PredicateArguments(movingStats, currentStats.getMean());
+				if (predicate.apply(arguments)) {
+					TimeStats<E> timeStats = new TimeStats<E>();
+					timeStats.add(times.getKey(), times.getValue());
+					filtered.put(entries.getKey(), timeStats);
+				}
+			}
+		}
+		return filtered;
 	}
 
 	@JsonIgnore
