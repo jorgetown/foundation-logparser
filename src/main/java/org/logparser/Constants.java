0c63ee66b10d834829143fca25915091b3cc1e42
jdecastro <jorgetown@gmail.com>
2010-09-11 09:55:04 +0100

    Made final

--- src/main/java/org/logparser/Constants.java
+++ src/main/java/org/logparser/Constants.java
@@ -6,7 +6,7 @@ package org.logparser;
  * @author jorge.decastro
  * 
  */
-public class Constants {
+public final class Constants {
 	public static final String FILE_SEPARATOR = System.getProperty("file.separator");
 	public static final String LINE_SEPARATOR = System.getProperty("line.separator");
 	public static final String CSV_VALUE_SEPARATOR = ",";
