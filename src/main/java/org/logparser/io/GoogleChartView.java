aacc02e79e0310f1c688bbed978a00c6f8e43517
jdecastro <jorgetown@gmail.com>
2010-09-19 18:27:57 +0100

    Introduced dependency to chart params collaborator, since it validates all inputs and has specific responsibilites such as encoding strings; introduced first-cut logic to generate markers automatically (needs lots of work)

--- src/main/java/org/logparser/io/GoogleChartView.java
+++ src/main/java/org/logparser/io/GoogleChartView.java
@@ -19,6 +19,7 @@ import org.apache.commons.math.stat.descriptive.StatisticalSummary;
 import org.apache.commons.math.stat.descriptive.SummaryStatistics;
 import org.apache.log4j.Logger;
 import org.logparser.LogEntry;
+import org.logparser.config.ChartParams;
 import org.logparser.stats.DayStats;
 import org.logparser.stats.TimeStats;
 
@@ -38,29 +39,20 @@ public class GoogleChartView {
 	private static final Logger LOGGER = Logger.getLogger(GoogleChartView.class.getName());
 	private final String baseUri;
 	private final Map<String, String> params;
+	private final ChartParams chartParams;
 
-	public GoogleChartView(final String baseUri, final Map<String, String> params) {
-		Preconditions.checkNotNull(baseUri, "'baseUri' argument cannot be null.");
-		Preconditions.checkNotNull(params, "'params' argument cannot be null.");
-		this.baseUri = baseUri;
-		this.params = params;
+	public GoogleChartView(final ChartParams chartParams) {
+		Preconditions.checkNotNull(chartParams, "'chartParams' argument cannot be null.");
+		this.chartParams = chartParams;
+		this.baseUri = chartParams.getBaseUri();
+		this.params = chartParams.getParams();
 	}
 
-	Function<String, URL> makeUrl = new Function<String, URL>() {
+	private final Function<String, URL> makeUrl = new Function<String, URL>() {
 		public URL apply(final String url) {
 			try {
 				return new URL(url);
 			} catch (MalformedURLException mue) {
-			}
-			return null;
-		}
-	};
-
-	Function<String, String> encodeString = new Function<String, String>() {
-		public String apply(final String str) {
-			try {
-				return URLEncoder.encode(str, "UTF-8");
-			} catch (UnsupportedEncodingException uee) {
 			}
 			return null;
 		}
@@ -82,11 +74,13 @@ public class GoogleChartView {
 			}
 		}
 	}
-
-	public Map<String, String> createChartUrls(final DayStats<LogEntry> dayStats) {
+	
+	// @TODO Oi. How did you get so big and clumsy?
+	public Map<String, String> createChartUrls(final DayStats<LogEntry> dayStats, final Map<String, TimeStats<LogEntry>> alerts) {
 		Map<String, String> urls = new HashMap<String, String>();
-
-		Map<String, String> encodedParams = Maps.transformValues(params, encodeString);
+		
+		String marker = params.remove("chm");
+		String statsMarker = marker;
 
 		for (Entry<String, TimeStats<LogEntry>> entries : dayStats.getDayStats().entrySet()) {
 
@@ -95,17 +89,35 @@ public class GoogleChartView {
 			List<Integer> labels = new ArrayList<Integer>();
 			url.append("chtt=");
 			url.append(entries.getKey());
+			
+			SummaryStatistics stats = new SummaryStatistics();
+			int index = 0;
+			for (Entry<Integer, StatisticalSummary> timeStats : entries.getValue().getTimeStats().entrySet()) {
+				means.add(timeStats.getValue().getMean());
+				labels.add(timeStats.getKey());
+				stats.addValue(timeStats.getValue().getMean());
+				
+				if (alerts.containsKey(entries.getKey())) {
+					TimeStats<LogEntry> alertStats = alerts.get(entries.getKey());
+					if (alertStats.getTimeStats().containsKey(timeStats.getKey())) {
+						statsMarker = String.format("%s|a,00E741,0,%s,18,-1", statsMarker, index);
+					}
+				}
+				index++;
+			}
+			url.append("&chm=");
+			try {
+				url.append(URLEncoder.encode(statsMarker.equals(marker)?marker:statsMarker, "UTF-8"));
+			} catch (UnsupportedEncodingException uee) {
+			}
+			statsMarker = marker;
+			
+			Map<String, String> encodedParams = chartParams.urlEncodeValues(params);
 			for (Entry<String, String> entry : encodedParams.entrySet()) {
 				url.append("&");
 				url.append(entry.getKey());
 				url.append("=");
 				url.append(entry.getValue());
-			}
-			SummaryStatistics stats = new SummaryStatistics();
-			for (Entry<Integer, StatisticalSummary> timeStats : entries.getValue().getTimeStats().entrySet()) {
-				means.add(timeStats.getValue().getMean());
-				labels.add(timeStats.getKey());
-				stats.addValue(timeStats.getValue().getMean());
 			}
 
 			long upperbound = Math.round(stats.getMax() + stats.getMin());
