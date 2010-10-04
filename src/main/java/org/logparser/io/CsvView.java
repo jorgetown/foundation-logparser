12baa89ca45202f575c2510364a0abdf2b2f2ed0
jdecastro <jorgetown@gmail.com>
2010-10-04 23:49:31 +0100

    Simplified csv generator API by allowing serializables to submit themselves

--- src/main/java/org/logparser/io/CsvView.java
+++ src/main/java/org/logparser/io/CsvView.java
@@ -6,6 +6,7 @@ import static org.logparser.Constants.LINE_SEPARATOR;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,27 +17,46 @@ import com.google.common.base.Preconditions;
 import com.google.common.io.Closeables;
 
 /**
- * Serializes data in CSV format.
+ * Serializes data in CSV format. Data must be {@link ICsvSerializable}.
  * 
  * @author jorge.decastro
  * 
  */
 public class CsvView {
 	private static final Logger LOGGER = Logger.getLogger(CsvView.class.getName());
+	private static final String DEFAULT_CSV_FILENAME = "log-analysis.csv";
+	private final String path;
+	private final String filename;
+	private final List<ICsvSerializable<?>> submitted;
 
-	public CsvView() {
+	public CsvView(final String path) {
+		this(path, DEFAULT_CSV_FILENAME);
 	}
 
-	public void write(final String path, final String filename, final ICsvSerializable<?>... csvSerializables) {
-		Preconditions.checkNotNull(path);
-		Preconditions.checkNotNull(filename);
-		Preconditions.checkNotNull(csvSerializables);
+	private CsvView(final String path, final String filename) {
+		this.path = Preconditions.checkNotNull(path, "'path' argument cannot be null.");
+		this.filename = Preconditions.checkNotNull(filename, "'filename' argument cannot be null.");
+		this.submitted = new ArrayList<ICsvSerializable<?>>();
+	}
+
+	public void submit(final ICsvSerializable<?> toSerialize) {
+		if (toSerialize != null) {
+			submitted.add(toSerialize);
+		}
+	}
+
+	public void write() {
+		write(path, filename, submitted);
+	}
+
+	public void write(final ICsvSerializable<?>... csvSerializables) {
+		Preconditions.checkNotNull(csvSerializables, "'csvSerializables' cannot be null.");
 
 		write(path, filename, Arrays.asList(csvSerializables));
 	}
 
 	private void write(final String path, final String filename, final List<ICsvSerializable<?>> csvSerializables) {
-		String filepath = String.format("%s%s%s.csv", path, FILE_SEPARATOR, filename);
+		String filepath = String.format("%s%s%s", path, FILE_SEPARATOR, filename);
 
 		LOGGER.info(String.format("Writing CSV file '%s'", filepath));
 
